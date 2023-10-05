55579a662cf9e91462816c84d7dbaad4abc25eb3
antonio <aserrano@itpfp.com>
2023-10-05 11:11:10 +0200

    taller7_commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 app.get('/', function (req, res){
     // res.send('hola mundo')
-    res.sendFile(path.resolve(__dirname, 'index2.html'))
+    res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
 app.listen(3000)
