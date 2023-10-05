dd1245acd66d24e30e90cadbb478e557da713969
antonio <aserrano@itpfp.com>
2023-10-05 11:17:20 +0200

    taller11_commit

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const path = require('path')
 
 app.get('/', function (req, res){
     // res.send('hola mundo')
-    res.sendFile(path.resolve(__dirname, 'index.html'))
-    res.sendFile(path.resolve(__dirname, '/Proy_001_TallerCoche/CSS/style2.css'))
+    res.sendFile(path.resolve(__dirname, 'index.html, /Proy_001_TallerCoche/CSS/style2.css'))
 })
 
 app.listen(3000)
